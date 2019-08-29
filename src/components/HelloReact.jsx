90a6d99c82fa20c9f7567d22ac2a62df703a95cf
Katherine Aguilera <katherinam35@gmail.com>
2019-08-29 16:57:09 -0500

    Entorno con React y webpack

---
+++ src/components/HelloReact.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const HelloReact = () => (
+  <h1>Hola desde React n_n</h1>
+)
+export default HelloReact;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloReact from './components/HelloReact'
 // import HelloWorld from './components/HelloWorld';
 
-ReactDOM.render(<h1>Hola</h1>, document.getElementById(app));
+ReactDOM.render(<HelloReact/>, document.getElementById('app'));

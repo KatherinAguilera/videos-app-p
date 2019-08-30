47b2335e83a5eed3029ee07fa47939ed4c521429
Katherine Aguilera <katherinam35@gmail.com>
2019-08-30 17:17:14 -0500

    Estructura Header

--- src/components/HelloReact.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import '../assets/styles/App.scss';
-
-const HelloReact = () => (
-  <h1>Hola desde React n_n</h1>
-)
-export default HelloReact;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloReact from './components/HelloReact'
-// import HelloWorld from './components/HelloWorld';
+import App from './container/App';
 
-ReactDOM.render(<HelloReact/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
